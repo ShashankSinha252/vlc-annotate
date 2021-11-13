0f9e4df7c24f6f65adb4053b7d57c977756ac094
Shashank Sinha <shashsinboy@gmail.com>
2021-11-14 00:28:59 +0530

    Experimental changes for dialog box
    Signed-off-by: Shashank Sinha <shashsinboy@gmail.com>

--- annotate.lua
+++ annotate.lua
@@ -6,17 +6,28 @@ function descriptor()
 		url = "https://github.com/ShashankSinha252/vlc-annotate";
 		shortdesc = "Annotate files by timeline in VLC";
 		description = "TBD";
+		capabilities = { "playing-listener" }
 	}
 end
 
 function activate()
-	dlg = vlc.dialog("Annotate")
+	return
+	-- dlg = vlc.dialog("Annotate")
 
-	-- Add content for dialog box
-	local row = 1
-	btn_toggle_playback = dlg:add_button("Play / Pause", toggle_playback, 1, row, 1, 1)
+	-- -- Add content for dialog box
+	-- local row = 1
+	-- btn_toggle_playback = dlg:add_button("Play / Pause", toggle_playback, 1, row, 1, 1)
 
-	dlg:update()
+	-- row = row + 1
+	-- local init_html_data =
+	-- [[
+	-- <style type="text/css">
+	-- body {background-color:white;}
+	-- </style>
+	-- ]]
+	-- box_html = dlg:add_html(init_html_data, 1, row, 1, 1)
+
+	-- dlg:update()
 end
 
 function close()
@@ -33,3 +44,22 @@ function toggle_playback()
 		end
 	end
 end
+
+function playing_changed()
+	vlc.msg.dbg("[Dummy] Status: " .. vlc.playlist.status())
+	-- dlg:delete()
+	dlg = vlc.dialog("Add Annotation")
+
+	-- Add content for dialog box
+	local row = 1
+	local init_html_data =
+	[[
+	<style type="text/css">
+	body {background-color:white;}
+	</style>
+	<div>Testing testing....</div>
+	]]
+	box_html = dlg:add_html(init_html_data, 1, row, 1, 1)
+
+	dlg:update()
+  end
